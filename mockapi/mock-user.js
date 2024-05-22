76cca34aee53cebb0045c4b1893617d4ad72fbce
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-05-22 23:49:17 +0200

    Created  mock users list to test CRUD

---
+++ mockapi/mock-user.js
@@ -0,0 +1,17 @@
+const Users = [
+  {
+    id: 1,
+    username: 'Jessica Drake',
+    email: "jss@example.com",
+    password: "123456",
+    favoriteMovies: ["The Dark Knight", "The Godfather", "The Shawshank Redemption"]
+  },
+  {
+    id: 2,
+    username: 'Ben Cohen',
+    password: "123456",
+    email: "ben@example.com",
+    favoriteMovies: []
+  }
+]
+module.exports = Users;

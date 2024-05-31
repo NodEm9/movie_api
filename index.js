68aa8db2920cde00a217a90b4d48a66526d118b8
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-05-31 09:14:39 +0200

    Implement validation mechenicism on addUsers and updateUsers entpoints

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express"),
   morgan = require('morgan'),
   fs = require('fs'),
-  path = require('path');
-
+  path = require('path'),
+  cors = require('cors');
+const { check } = require('express-validator');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/movieDB');
-let movies = require("./api/movies.js");
-let users = require("./api/users.js");
+let movies = require("./controllers/movies.js");
+let users = require("./controllers/users.js");
 
 // Create an instance of express
 var app = express();
+
+const port = process.env.PORT || 8080;
 
 // Create a write stream (in append mode)
 const accesLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { flags: "a" });
@@ -19,34 +22,50 @@ const accesLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { f
 app.use(morgan("combined", { stream: accesLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-let auth = require("./api/auth/auth")(app);
+let auth = require("./controllers/auth/auth")(app);
 let passport = require('passport');
-require('./api/auth/passport');
-   
+require('./controllers/auth/passport');
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => { 
+app.get("/", (req, res) => {
   let responseText = "<h1 style='text-align: center; padding: 50;'>" +
     'Welcome to Movie Oasis!' + "</h1>";
   res.send(responseText)
 });
 
 // Movies routes
-app.get("/movies", passport.authenticate("jwt", {session: false}), movies.getMovies);
-app.get("/movies/:title", passport.authenticate("jwt", { session: false}), movies.getMovieByTitle);
-app.get("/movies/genre/:genreName", passport.authenticate("jwt", { session: false}), movies.getGenreByName);
-app.get("/movies/director/:directorName", passport.authenticate("jwt", { session: false}), movies.getDirectorByName);
+app.get("/movies", passport.authenticate("jwt", { session: false }), movies.getMovies);
+app.get("/movies/:title", passport.authenticate("jwt", { session: false }), movies.getMovieByTitle);
+app.get("/movies/genre/:genreName", passport.authenticate("jwt", { session: false }), movies.getGenreByName);
+app.get("/movies/director/:directorName", passport.authenticate("jwt", { session: false }), movies.getDirectorByName);
 
 
 // Users routes
-app.get("/users", passport.authenticate("jwt", { session: false}), users.getUsers);
-app.post("/users", users.addUser);
-app.get("/users/:Username", passport.authenticate("jwt", { session: false}), users.getUserByUsername);
-app.put("/users/:Username", passport.authenticate("jwt", { session: false}), users.updateUser);
-app.post("/users/:Username/movies/:MovieID", passport.authenticate("jwt", { session: false}), users.addFavoriteMovie);
-app.delete("/users/:Username", passport.authenticate("jwt", { session: false}), users.deleteUser);
-app.delete("/users/:Username/movies/:MovieID", passport.authenticate("jwt", { session: false}), users.removeFavoriteMovie);
+app.get("/users", passport.authenticate("jwt", { session: false }), users.getUsers);
+
+app.post("/users", [
+  check("Username", "Username is required").isLength({ min: 5 }),
+  check("Username", "Username contains non alphanumeric characters - not allowed").isAlphanumeric(),
+  check("Password", "Password is required").not().isEmpty(),
+  check("Email", "Email does not appear to be valid").isEmail()
+], users.addUser);
+
+app.get("/users/:Username", passport.authenticate("jwt", { session: false }), users.getUserByUsername);
+
+app.put("/users/:Username", [
+  check("Username", "Username is required").isLength({ min: 5 }),
+  check("Username", "Username contains non alphanumeric characters - not allowed").isAlphanumeric(),
+  check("Password", "Password is required").not().isEmpty(),
+  check("Email", "Email does not appear to be valid").isEmail()
+], passport.authenticate("jwt", { session: false }),
+  users.updateUser);
+
+app.post("/users/:Username/movies/:MovieID", passport.authenticate("jwt", { session: false }), users.addFavoriteMovie);
+app.delete("/users/:Username", passport.authenticate("jwt", { session: false }), users.deleteUser);
+app.delete("/users/:Username/movies/:MovieID", passport.authenticate("jwt", { session: false }), users.removeFavoriteMovie);
 
 
 // Get the documentation
@@ -61,6 +80,6 @@ app.use((err, req, res, next) => {
 });
 
 // Listen for requests
-app.listen(8080, () => {
-  console.log("Server is running on port 8080")
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`)
 });

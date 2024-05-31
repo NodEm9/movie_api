e5755ef2b0d9926d7449ff8a6f044f3bde677484
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-05-31 22:37:16 +0200

    change environment variable name

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGO_URI);
 // mongoose.connect('mongodb://localhost:27017/movieDB'); // for local testing
 
 let Models = require("../model/models.js");

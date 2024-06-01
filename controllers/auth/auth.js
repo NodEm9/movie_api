2e1b88a98e0cab74e138ef3ed8373a9077ae3304
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-01 12:44:10 +0200

    Add eslint inline rules  for no-def to allow environmentt variable usage

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -12,13 +12,13 @@ let generateJWTToken = (user) => {
     subject: user.Username,
     expiresIn: "1d",
     algorithm: "HS256" 
-  })
+  }) /* eslint no-undef: off */ 
 };
 
 // POST login
 module.exports = (router) => {
   router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
+    passport.authenticate("local", { session: false }, (error, user) => {
       if (error || !user) {
         return res.status(400).json({
           message: "Something is not right.",

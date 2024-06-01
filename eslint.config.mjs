b88b830879cadba06ca27ff9cc3b8a3cf2354931
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-01 12:49:16 +0200

    initialize eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
-module.exports = [
+export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  rules: {
-    "no-console": "off",
-    "no-unused-vars": "off",
-    "no-undef": "off",
-    "no-constant-condition": "off",
-    "no-irregular-whitespace": "error",
-    "no-prototype-builtins": "off",
-    "no-async-promise-executor": "error",
-    "no-var": "error",
-    semi: ["error", "always"],
-    quotes: ["error", "single"]
-  }
 ];

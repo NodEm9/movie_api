ea40632802807eccdbad7eb5d0b25318d25d670b
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-17 13:04:24 +0200

    Refactor add user response error

--- controllers/users.js
+++ controllers/users.js
@@ -42,7 +42,7 @@ async function addUser(req, res) {
           Birthday: req.body.Birthday,
           Role: req.body.Role
         })
-          .then(user => res.status(201).json({ message: `Account ${user} was created.` }, user))
+          .then(user => res.status(201).json({ message: `Account ${user} was created.` }))
           .catch(err => {
             res.status(500).send("Error: " + err)
           })

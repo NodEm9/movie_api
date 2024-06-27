75f61dac550c1a73c6f1530c685195dd29706dcc
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-06-27 09:15:15 +0200

    Add Domain to allowed origins

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: (origin, callback) => {
     if (process.env.ALLOWED_ORIGINS.split(',').indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true) 
     } else {
       var msg = "The CORS policy for this site does not allow access from the specified Origin.";
       return callback(new Error(msg), false);
